670c3eb3c775722fcdc53baddce3bf4022eeee32
nonota7788 <f.t.fs0924@gmail.com>
2020-04-27 00:27:33 +0900

    Updates

---
+++ precache-manifest.e67eb0b65eb3cfd3f26d23a57c1e83e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a38dde92e784f658c1633b2ee288cf6",
+    "url": "/vidly/index.html"
+  },
+  {
+    "revision": "10e1f2370ba2d9b3e43a",
+    "url": "/vidly/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "0a19db661121c29100a9",
+    "url": "/vidly/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "10e1f2370ba2d9b3e43a",
+    "url": "/vidly/static/js/2.3db19a7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vidly/static/js/2.3db19a7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a19db661121c29100a9",
+    "url": "/vidly/static/js/main.407a0e8f.chunk.js"
+  },
+  {
+    "revision": "89e6acde68c710b0d378",
+    "url": "/vidly/static/js/runtime-main.9cf53b91.js"
+  }
+]);
